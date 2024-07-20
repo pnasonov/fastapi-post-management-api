990ef0124a47c74fdd0b776d997d1909a5765c4f
pnasonov <pavlonasonov@gmail.com>
2024-07-20 18:36:04 +0300

    refactor: add user mixin

--- core/models/post.py
+++ core/models/post.py
@@ -1,23 +1,24 @@
 from typing import TYPE_CHECKING
 
-from sqlalchemy import Text, String, ForeignKey
+from sqlalchemy import Text, String
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 
 from core.models.base import Base
+from core.models.mixins import UserRelationMixin
 
 if TYPE_CHECKING:
-    from core.models.user import User
+    from core.models.commentary import Commentary
 
 
-class Post(Base):
+class Post(UserRelationMixin, Base):
     __tablename__ = "posts"
+
+    _user_back_populates = "posts"
 
     title: Mapped[str] = mapped_column(String(100))
     description: Mapped[str] = mapped_column(
         Text, nullable=True, default="", server_default=""
     )
-
-    user_id: Mapped[int] = mapped_column(
-        ForeignKey("users.id"),
+    commentaries: Mapped[list["Commentary"]] = relationship(
+        back_populates="post",
     )
-    user: Mapped["User"] = relationship(back_populates="posts")
