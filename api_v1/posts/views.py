3714717bd5389ecfa8bf1d8250023b6d8b2b24b7
pnasonov <pavlonasonov@gmail.com>
2024-07-20 10:55:43 +0300

    feat: create patch and delete view for posts

--- api_v1/posts/views.py
+++ api_v1/posts/views.py
@@ -1,9 +1,14 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import db_helper
 from api_v1.posts import crud
-from api_v1.posts.schemas import Post, PostCreate, PostUpdate
+from api_v1.posts.schemas import (
+    Post,
+    PostCreate,
+    PostUpdate,
+    PostUpdatePartial,
+)
 from api_v1.posts import dependencies
 
 router = APIRouter(prefix="/api/posts", tags=["posts"])
@@ -16,7 +21,7 @@ async def list_posts(
     return await crud.get_posts(session=session)
 
 
-@router.post("/", response_model=Post)
+@router.post("/", response_model=Post, status_code=status.HTTP_201_CREATED)
 async def create_post(
     post: PostCreate,
     session: AsyncSession = Depends(db_helper.session_dependency),
@@ -40,3 +45,22 @@ async def update_post(
     return await crud.update_post(
         session=session, post_db=post, post_update=post_update
     )
+
+
+@router.patch("/{post_id}", response_model=Post)
+async def update_post_partial(
+    post_update: PostUpdatePartial,
+    post: Post = Depends(dependencies.get_post_by_id),
+    session: AsyncSession = Depends(db_helper.session_dependency),
+):
+    return await crud.update_post(
+        session=session, post_db=post, post_update=post_update, partial=True
+    )
+
+
+@router.delete("/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post(
+    post: Post = Depends(dependencies.get_post_by_id),
+    session: AsyncSession = Depends(db_helper.session_dependency),
+):
+    await crud.delete_post(session=session, post_db=post)
