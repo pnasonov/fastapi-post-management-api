3b05f6da1b4f9f67fcd08e71fee159ea20f6be05
pnasonov <pavlonasonov@gmail.com>
2024-07-20 14:12:12 +0300

    feat: remove create all on app start

--- main.py
+++ main.py
@@ -2,15 +2,12 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from core.models import Base, db_helper
 from users.views import router as users_router
 from api_v1 import router as posts_router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
     yield
 
 
