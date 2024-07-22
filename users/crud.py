573aff935eed871a74081635af90f3e7a397d24b
pnasonov <pavlonasonov@gmail.com>
2024-07-22 19:13:38 +0300

    refactor: fix project structure for user app

--- users/crud.py
+++
@@ -1,9 +0,0 @@
-from users.schemas import CreateUser
-
-
-def create_user(user: CreateUser):
-    db_user = user.model_dump()
-    return {
-        "success": True,
-        "user": db_user,
-    }
--- users/schemas.py
+++
@@ -1,9 +0,0 @@
-from typing import Annotated
-from annotated_types import MaxLen, MinLen
-
-from pydantic import BaseModel, EmailStr
-
-
-class CreateUser(BaseModel):
-    username: Annotated[str, MinLen(3), MaxLen(20)]
-    email: EmailStr
