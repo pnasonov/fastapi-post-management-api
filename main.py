31518adf271b200107266c051b3f3e39c5f0cd93
pnasonov <pavlonasonov@gmail.com>
2024-07-23 13:15:03 +0300

    refactor: remove redundant router

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from core.config import settings
-from users.views import router as users_router
 from api_v1 import router as router_v1
 
 
@@ -14,4 +13,3 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(router_v1, prefix=settings.api_v1_prefix)
-app.include_router(users_router)
